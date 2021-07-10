f027b19eb54dda000181dd0ef0bccf681ef01785
Ryuno-studylog <52373920+Ryuno-studylog@users.noreply.github.com>
2021-07-11 00:09:02 +0900

    modelの修正、希望シフトの登録

--- Django/Kerberos/api/functions.py
+++ Django/Kerberos/api/functions.py
@@ -8,9 +8,33 @@ import json
 
 #送られてきたシフトを登録する
 def submit_rest_requests(input_data:dict):
-    #new_rest_request = RestRequest.objects.create()
+    kerbero_id = input_data["kerbero_id"]
+    head_name = input_data["head_name"]
 
-    #make_schedules(input_data)
+    rest_requests = RestRequest.objects.filter(kerbero_id=kerbero_id,head_name=head_name,date__gte=date.today())
+
+    for req in input_data["request"]:
+        req_date = datetime.strptime(req["date"],'%Y-%m-%d').date()
+        requested = False
+
+        flag = 0
+        for checked_val in rest_requests:
+            if checked_val.date == req_date:
+                flag = 1
+                break
+        if flag:
+            continue
+
+        if req["request"]:
+            requested = True
+            start_time = datetime.strptime(req["start_time"],"%H:%M").time()
+            end_datetime = make_end_datetime(req_date,req["end_time"])
+            RestRequest.objects.create(kerbero_id=kerbero_id,head_name=head_name,requested=requested,date=req_date,start_time=start_time,end_datetime=end_datetime)
+        else:
+            RestRequest.objects.create(kerbero_id=kerbero_id,head_name=head_name,requested=requested,date=req_date)
+
+        make_schedules(rest_requests,input_data)
+        
     return input_data
 
 #DBから該当の日付を持ってきて出力する
@@ -20,6 +44,25 @@ def get_scheduls(input_data:dict):
     return val
 
 
-def make_schedules(input_data:dict):
+def make_schedules(rest_requests,input_data:dict):
     val = ""
     return val
+
+
+
+
+def make_end_datetime(req_date,end_time:str):
+    return_date = req_date
+    end_time_hour = int(end_time[:end_time.find(':')])  # スライスで半角空白文字よりも前を抽出
+    if end_time[(end_time.find(':')+1):]=="00" or end_time[(end_time.find(':')+1):]=="0":
+        end_time_minute = 0
+    else:
+        end_time_minute = int(end_time[(end_time.find(':')+1):])
+
+    print(return_date)
+    print(end_time_hour)
+    if end_time_hour >= 24:
+        return_date = date(req_date.year,req_date.month,req_date.day+1)
+        end_time_hour -= 24
+        
+    return datetime(return_date.year,return_date.month,return_date.day,end_time_hour,end_time_minute)
--- Django/Kerberos/api/urls.py
+++ Django/Kerberos/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registre_rest_request/', views.register_rest_requests),
+    path('register_rest_request/', views.register_rest_requests),
 #    path('option/', views.OptionAPIView.as_view()),
 #    path('cast/', views.CastAPIView.as_view()),
 #    path('callback/',views.callback),
